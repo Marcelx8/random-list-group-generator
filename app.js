98f9391baa45cdb1bf9dd4434a7abd1ac6c1dee8
msteenkamp <marcel@zailab.com>
2018-08-09 16:56:12 +0200

    create basic list of people

--- app.js
+++ app.js
@@ -1,1 +1,45 @@
 console.log(':: Random List Group Generator ::');
+// console.log();
+
+let person = {
+    name: 'Piet',
+    surname: 'Takkie'
+};
+
+let people = [{
+        name: 'Marcel',
+        surname: 'Steenkamp'
+    },
+    {
+        name: 'Fred',
+        surname: 'du Plessis'
+    },
+    {
+        name: 'Martea',
+        surname: 'Roos'
+    },
+    {
+        name: 'Michela',
+        surname: 'Webster'
+    },
+    {
+        name: 'Karin',
+        surname: 'Uys'
+    },
+    {
+        name: 'Piet',
+        surname: 'Takkie'
+    }
+];
+
+for (let index = 0; index < people.length; index++) {
+
+    let listItem = document.createElement('li');
+    listItem.setAttribute('class', 'list-item');
+
+    let listWrapper = document.querySelector('.list-wrapper');
+    listItemContent = document.createTextNode(`${people[index].name} ${people[index].surname}`);
+
+    listItem.appendChild(listItemContent);
+    listWrapper.appendChild(listItem);
+}
